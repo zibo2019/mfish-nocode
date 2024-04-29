57794efd2a0f247664c83c56d5a51992507e0551
zibo2019 <49141344+zibo2019@users.noreply.github.com>
2024-04-29 16:13:22 +0800

    feat(code): FreeMarker 代码注释与优化

--- mf-common/mf-common-code/src/main/java/cn/com/mfish/common/code/common/FreemarkerUtils.java
+++ mf-common/mf-common-code/src/main/java/cn/com/mfish/common/code/common/FreemarkerUtils.java
@@ -22,20 +22,29 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import jakarta.annotation.Resource;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 /**
  * @author: mfish
@@ -46,6 +55,7 @@ import java.util.zip.ZipOutputStream;
 @Slf4j
 @RefreshScope
 public class FreemarkerUtils {
+
     @Resource
     Configuration fmConfig;
     @Resource
@@ -60,55 +70,92 @@ public class FreemarkerUtils {
     /**
      * 初始化请求参数
      *
-     * @param reqCode
-     * @return
+     * @param reqCode 请求参数
      */
+    @SuppressWarnings("all") // 缓解强迫症
     public void initReqCode(ReqCode reqCode) {
+        // 校验数据库连接ID
         if (StringUtils.isEmpty(reqCode.getConnectId())) {
             throw new MyRuntimeException("错误:库名不允许为空");
         }
+
+        // 校验表名
         if (StringUtils.isEmpty(reqCode.getTableName())) {
             throw new MyRuntimeException("错误:表名不允许为空");
         }
+
+        // 校验包名
+        // 包名为空时默认为cn.com.mfish.web
         if (StringUtils.isEmpty(reqCode.getPackageName())) {
             reqCode.setPackageName("cn.com.mfish.web");
-        } else if (!StringUtils.isMatch("^([A-Za-z]{1}[A-Za-z\\d_]*\\.?)+[A-Za-z][A-Za-z\\d_]*$", reqCode.getPackageName())) {
+        }
+
+        // 包名不为空时，校验包名格式
+        else if (!StringUtils.isMatch("^([A-Za-z]{1}[A-Za-z\\d_]*\\.?)+[A-Za-z][A-Za-z\\d_]*$", reqCode.getPackageName())) {
             throw new MyRuntimeException("错误:包名格式不规范");
         }
+
+        // 校验接口前缀
         if (StringUtils.isEmpty(reqCode.getApiPrefix())) {
+            // 如果apiPrefix为空，则尝试从packageName中提取apiPrefix
             int index = reqCode.getPackageName().lastIndexOf(".");
             if (index >= 0) {
+                // 如果包名中包含点，则提取点之后的部分作为apiPrefix
                 reqCode.setApiPrefix(reqCode.getPackageName().substring(index + 1));
             } else {
+                // 如果包名中不包含点，则直接使用包名作为apiPrefix
                 reqCode.setApiPrefix(reqCode.getPackageName());
             }
-        } else if (!StringUtils.isMatch("^[A-Za-z]+[A-Za-z\\d_]*$", reqCode.getApiPrefix())) {
+        }
+
+        // 如果apiPrefix不为空，则检查apiPrefix的格式是否符合规范
+        // 规范要求apiPrefix以字母开头，后面可以跟字母、数字或下划线
+        else if (!StringUtils.isMatch("^[A-Za-z]+[A-Za-z\\d_]*$", reqCode.getApiPrefix())) {
             throw new MyRuntimeException("错误:接口前缀格式不规范");
         } else {
+            // 如果格式符合规范，则将apiPrefix转为小写
             reqCode.setApiPrefix(reqCode.getApiPrefix().toLowerCase());
         }
+
+        // 校验实体名
         if (StringUtils.isEmpty(reqCode.getEntityName())) {
+            // 如果entityName为空，则使用表名作为entityName
             reqCode.setEntityName(reqCode.getTableName());
         } else if (!StringUtils.isMatch("^[A-Za-z]+[A-Za-z\\d_]*$", reqCode.getEntityName())) {
             throw new MyRuntimeException("错误:实体类名格式不规范");
         }
-        //如果entityName带下划线，转为首字母大写驼峰，否则其他不变首字母大写
-        if (reqCode.getEntityName().indexOf("_") > 0) {
+        // 如果entityName带下划线，转为首字母大写驼峰，否则其他不变首字母大写
+        if (reqCode.getEntityName().contains("_")) {
             reqCode.setEntityName(StringUtils.toCamelBigCase(reqCode.getEntityName()));
         } else {
             reqCode.setEntityName(StringUtils.firstUpperCase(reqCode.getEntityName()));
         }
+
+        // 检查表注释是否为空
         if (StringUtils.isEmpty(reqCode.getTableComment())) {
+            // 如果tableComment为空，则将表名作为表描述
             reqCode.setTableComment(reqCode.getTableName());
-            Result<PageResult<TableInfo>> result = remoteDbConnectService.getTableList(RPCConstants.INNER, reqCode.getConnectId(), reqCode.getTableName(), new ReqPage().setPageNum(1).setPageSize(10000));
+
+            // 从远程数据库服务获取表列表
+            Result<PageResult<TableInfo>> result =
+                remoteDbConnectService.getTableList(RPCConstants.INNER, reqCode.getConnectId(), reqCode.getTableName(),
+                    new ReqPage().setPageNum(1).setPageSize(10000));
+
+            // 如果结果不成功，抛出自定义异常
             if (!result.isSuccess()) {
                 throw new MyRuntimeException(result.getMsg());
             }
+
+            // 如果结果数据为空或列表为空，抛出自定义异常
             if (result.getData() == null || result.getData().getList() == null || result.getData().getList().isEmpty()) {
                 throw new MyRuntimeException("错误:未获取到表信息");
             }
+
+            // 获取表信息
             TableInfo tableInfo = result.getData().getList().get(0);
-            if (tableInfo != null && !StringUtils.isEmpty(tableInfo.getTableComment())) {
+
+            // 如果表信息不为空且表注释不为空，则更新表注释
+            if (tableInfo != null && StringUtils.isNotEmpty(tableInfo.getTableComment())) {
                 reqCode.setTableComment(tableInfo.getTableComment());
             }
         }
@@ -117,16 +164,23 @@ public class FreemarkerUtils {
     /**
      * 获取代码
      *
-     * @param reqCode
-     * @return
+     * @param reqCode 请求参数
+     * @return 代码列表
      */
     public List<CodeVo> getCode(ReqCode reqCode) {
+        // 初始化请求参数
         initReqCode(reqCode);
+
+        // 设置代码信息的包名、实体名和接口前缀
         CodeInfo codeInfo = new CodeInfo();
         codeInfo.setPackageName(reqCode.getPackageName());
         codeInfo.setEntityName(reqCode.getEntityName());
         codeInfo.setApiPrefix(reqCode.getApiPrefix());
-        Result<PageResult<FieldInfo>> result = remoteDbConnectService.getFieldList(RPCConstants.INNER, reqCode.getConnectId(), reqCode.getTableName(), new ReqPage().setPageNum(1).setPageSize(10000));
+
+        // 获取字段信息
+        Result<PageResult<FieldInfo>> result =
+            remoteDbConnectService.getFieldList(RPCConstants.INNER, reqCode.getConnectId(), reqCode.getTableName(),
+                new ReqPage().setPageNum(1).setPageSize(10000));
         if (!result.isSuccess()) {
             throw new MyRuntimeException(result.getMsg());
         }
@@ -134,10 +188,15 @@ public class FreemarkerUtils {
             throw new MyRuntimeException("错误:未获取到字段信息");
         }
         List<FieldInfo> list = result.getData().getList();
+
+        // 构建搜索字段信息
         codeInfo.setSearchList(buildSearch(reqCode.getSearches(), list));
+
+        // 初始化ID类型和扩展字段列表
         String idType = "";
         List<FieldExpand> expandList = new ArrayList<>();
-        //缺省字段字段不需要生成,获取列时过滤掉
+
+        // 过滤掉默认字段，这些字段不需要生成
         for (int i = 0; i < list.size(); i++) {
             FieldInfo fieldInfo = list.get(i);
             String fieldName = fieldInfo.getFieldName();
@@ -146,7 +205,7 @@ public class FreemarkerUtils {
             }
             fieldName = fieldName.toLowerCase(Locale.ROOT);
             if (DefaultField.values.contains(fieldName)) {
-                //设置唯一ID类型
+                // 设置唯一ID类型
                 if ("id".equals(fieldName)) {
                     idType = fieldInfo.getType();
                 }
@@ -155,18 +214,22 @@ public class FreemarkerUtils {
             }
             final String finalFieldName = fieldName;
             Optional<SearchInfo> searchInfo = codeInfo.getSearchList().stream().filter((search) ->
-                            search.getFieldInfo().getFieldName().equals(finalFieldName) && search.getComponent().size() == 2)
-                    .findFirst();
+                    search.getFieldInfo().getFieldName().equals(finalFieldName) && search.getComponent().size() == 2)
+                .findFirst();
             fieldInfo.setFieldName(StringUtils.toCamelCase(fieldName));
             FieldExpand fieldExpand = new FieldExpand().setFieldInfo(fieldInfo);
-            //设置字典组件列表
+            // 设置字典组件列表
             searchInfo.ifPresent(info -> fieldExpand.setDictComponent(info.getComponent()));
             expandList.add(fieldExpand);
         }
+
+        // 构建表扩展信息
         TableExpand tableExpand = new TableExpand();
         tableExpand.setColumns(list).setTableName(reqCode.getTableName())
-                .setTableComment(reqCode.getTableComment()).setIdType(idType);
+            .setTableComment(reqCode.getTableComment()).setIdType(idType);
         codeInfo.setTableInfo(tableExpand.setFieldExpands(expandList));
+
+        // 递归调用以获取最终的代码信息列表
         return getCode(codeInfo);
     }
 
@@ -182,9 +245,9 @@ public class FreemarkerUtils {
             }
             SearchInfo searchInfo = new SearchInfo();
             searchInfo.setFieldInfo(optional.get())
-                    .setField(StringUtils.toCamelBigCase(reqSearch.getField()))
-                    .setCondition(reqSearch.getCondition())
-                    .setComponent(reqSearch.getComponent());
+                .setField(StringUtils.toCamelBigCase(reqSearch.getField()))
+                .setCondition(reqSearch.getCondition())
+                .setComponent(reqSearch.getComponent());
             searchInfos.add(searchInfo);
         }
         return searchInfos;
@@ -315,8 +378,8 @@ public class FreemarkerUtils {
                 log.error("错误:文件保存异常", e);
             }
         }
-        //压缩样例，暂不压缩
-        //ZipUtils.toZip(path, true);
+        // 压缩样例，暂不压缩
+        // ZipUtils.toZip(path, true);
         return true;
     }
 
@@ -337,7 +400,7 @@ public class FreemarkerUtils {
                 zos.flush();
                 zos.closeEntry();
             }
-            //需要主动关闭后，再返回outputStream否则返回文件不全
+            // 需要主动关闭后，再返回outputStream否则返回文件不全
             IOUtils.closeQuietly(zos);
             return outputStream.toByteArray();
         } catch (IOException e) {
--- mf-common/mf-common-code/src/main/java/cn/com/mfish/common/code/config/properties/FreemarkerProperties.java
+++ mf-common/mf-common-code/src/main/java/cn/com/mfish/common/code/config/properties/FreemarkerProperties.java
@@ -11,15 +11,27 @@ import java.util.List;
 
 /**
  * @author: mfish
- * @description: freemarker模版keys配置
+ * @description: FreemarkerProperties 类用于配置和存储与 Freemarker 模板引擎相关的属性。
  * @date: 2022/8/30 16:15
  */
+@Data
 @Configuration
 @RefreshScope
 @ConfigurationProperties(prefix = "code.template")
-@Data
 public class FreemarkerProperties {
+    /**
+     * keys 列表用于存储模板的键值，这些键值可能用于模板的加载或配置。
+     */
     private final List<String> keys = new ArrayList<>();
-    private final String path = "template";
-    private final Version version = freemarker.template.Configuration.VERSION_2_3_31;
+
+    /**
+     * path 字段定义了模板文件存储的路径。
+     */
+    private String path;
+
+    /**
+     * version 字段指定了 Freemarker 模板引擎的版本。
+     * 这里使用了 Freemarker 的静态字段 VERSION_2_3_31，表示使用的是 2.3.31 版本。
+     */
+    private Version version = freemarker.template.Configuration.VERSION_2_3_31;
 }
